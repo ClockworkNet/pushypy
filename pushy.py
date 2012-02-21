73a0f97acb93aebbcf9d813738ee3f0d19b0caa4
Alex Barrett <barrett@clockwork.net>
2012-02-21 11:37:29 -0600

    Added file system operations

--- pushy.py
+++ pushy.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-import os, time, sys, re, logging, subprocess
+import os, time, sys, re, logging, shutil
 
 class FileMonitor(object):
 
@@ -23,23 +23,23 @@ class FileMonitor(object):
             for file in files:
                 file_path = os.path.join(root, file)
                 self.add_file(file_path)
-        logging.info("Tracking directories", self.dirs)
-        logging.info("Tracking files", self.files)
+        logging.info("Tracking directories: %s" % ", ".join(self.dirs))
+        logging.info("Tracking files: %s" % ", ".join(self.files))
 
 
     def should_ignore(self, path):
         path = os.path.abspath(path)
         if not os.path.exists(path):
-            logging.warn("ignored, path doesn't exist", path)
+            logging.warn("ignored, path %s doesn't exist" % path)
             return True
         elif re.search(self.ignored_dirs, path):
-            logging.info("ignored, matches dir pattern", path)
+            logging.info("ignored, path %s matches dir pattern" % path)
             return True
         elif os.path.isdir(path):
-            logging.info("okay, doesn't match dir pattern and is a directory", path)
+            logging.info("okay, path %s doesn't match dir pattern and is a directory" % path)
             return False
         elif re.search(self.ignored_files, path):
-            logging.info("ignored, matches file pattern", path)
+            logging.info("ignored, path %s matches file pattern" % path)
             return True
         else:
             return False 
@@ -128,51 +128,81 @@ class Event(object):
     __len__  = getHandlerCount
 
 
-class GitPush(object):
+class Pusher(object):
 
     def __init__(self, source, target):
-        self.source = source
-        self.target = target
+        self.source = os.path.abspath(source)
+        self.target = os.path.abspath(target)
 
 
     def add(self, item):
-        path = os.path.abspath(item)
-        if os.path.exists(path):
-            self.call("git add %s" % path)
+        source_path = os.path.abspath(item)
+        target_path = self.determine_destination(item)
+        if os.path.exists(target_path):
+            logging.error("Skipping add for '%s': file already exists" % item)
+        elif os.path.exists(source_path):
+            logging.info("Pushing %s to %s" % (source_path, target_path))
+            self.push(source_path, target_path)
         else:
-            logging.error("File doesn't exist for adding", path)
+            logging.error("Path %s doesn't exist for adding" % source_path)
+
+
+    def update(self, item):
+
+        source_path = os.path.abspath(item)
+        target_path = self.determine_destination(item)
+
+        if not os.path.exists(target_path):
+            logging.info("%s does not exist in target. Adding instead of updating." % item)
+            self.add(item)
+            return
+
+        source_mod  = os.stat(source_path).st_mtime
+        target_mod  = os.stat(target_path).st_mtime
+
+        if target_mod > source_mod:
+            logging.warn("Skipping update of %s on target. Target file is more recent that source file." % item)
+            return
+
+        logging.info("Updating %s to %s" % (source_path, target_path))
+        self.push(source_path, target_path)
 
 
     def remove(self, item):
-        path = os.path.abspath(item)
-        self.call("git rm %s" % path)
-
-
-    def commit(self, msg):
-        self.call('git commit -a -m "%s"' % msg.replace('"', '\\"'))
-
-
-    def export(self):
-        if not self.commit("Saving for export to %s" % self.target):
+        target_path = self.determine_destination(item)
+        
+        if not os.path.exists(target_path):
+            logging.info("Skipping remove. Target file %s doesn't exist." % target_path)
             return
-        # @TODO: implement this
-        destination = os.path.join(self.target, "backup.zip")
-        self.call("git archive --format=zip --output=%s" % destination)
 
-
-    def call(self, cmd):
-        print "calling '%s'" % cmd
+        logging.info("Removing %s from %s" % (source_path, target_path))
+        
         try:
-            r = subprocess.call(cmd, shell=True)
-            return True
-        except:
-            logging.error("Error calling command '%s'" % cmd)
-            return False
+            if os.path.isdir(target_path):
+                os.rmdir(target_path)
+            else:
+                os.remove(target_path)
+        except Error, err:
+            logging.error("Error occurred while removing %s: %s" % (item, str(err)))
+
+
+    def push(self, source_path, target_path):
+        try:
+            shutil.copy(source_path, target_path)
+        except Error, err:
+            logging.error("Error ocurred while copying file: %s" % str(err))
+
+
+    def determine_destination(self, item):
+        abs_path      = os.path.abspath(item)
+        relative_path = os.path.relpath(abs_path, self.source)
+        destination   = os.path.join(self.target, relative_path)
+        return destination
 
 
 class Main(object):
     def __init__(self, args):
-        dir    = os.path.abspath(args[0])
+        dir = os.path.abspath(args[0])
 
         self.monitor = FileMonitor(dir)
         self.monitor.file_changed += self.handle_change
@@ -180,7 +210,7 @@ class Main(object):
 
         if len(args) > 1:
             target = os.path.abspath(args[1])
-            self.pusher = GitPush(dir, target)
+            self.pusher = Pusher(dir, target)
 
         self.monitor.start()
 
@@ -188,9 +218,11 @@ class Main(object):
         print "file changed", path, action
         if action == "deleted":
             self.pusher.remove(path)
-        else:
+        elif action == "added":
             self.pusher.add(path)
-        self.pusher.export()
+        elif action == "updated":
+            self.pusher.update(path)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     Main(sys.argv[1:])
